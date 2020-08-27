3e266d7dab8c8e8f7515b53ad52178d33564ca22
benjamin <529709737@qq.com>
2020-08-27 14:46:43 +0800

    [test][Benjamin] refactor: extract method caculateAmount

--- src/statement.js
+++ src/statement.js
@@ -2,15 +2,27 @@ function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
+  const format = forUSDFormat();
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = caculateAmount(play, perf);
+
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // add extra credit for every ten comedy attendees
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    
+    //print line for this order
+    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits \n`;
+  return result;
+
+  
+  function caculateAmount(play, perf) {
     switch (play.type) {
       case 'tragedy':
         thisAmount = 40000;
@@ -28,19 +40,16 @@ function statement (invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`);
     }
-
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    
-    //print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    return thisAmount;
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits \n`;
-  return result;
+
+  function forUSDFormat() {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format;
+  }
 }
 
 module.exports = {
--- test/statementTest.js
+++ test/statementTest.js
@@ -94,6 +94,25 @@ test('test 4', t => {
   + "You earned 4 credits \n");
 });
 
+test('test 5', t => {
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'as-like',
+        'audience': 21,
+      }
+    ],
+  };
+
+  const result = statement(invoice, plays);
+  
+  t.is(result, "Statement for BigCo\n"
+  + " As You Like It: $468.00 (21 seats)\n"
+  + "Amount owed is $468.00\n"
+  + "You earned 4 credits \n");
+});
+
   
 const plays = {
   'hamlet': {
